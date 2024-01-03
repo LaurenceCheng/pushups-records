8bbe1484b64f949c03ce6307d739710d29f08c37
Chin-Yi Cheng <LaurenceCheng@users.noreply.github.com>
2024-01-03 18:22:42 +0800

    Update the size of the figure

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import calplot
 data = pandas.read_csv('pushup.csv', parse_dates=['date_time'])
 data = data.set_index('date_time')
 print(data)
-fig, ax = calplot.calplot(data['pushup'], cmap='YlGn', edgecolor='white')
+fig, ax = calplot.calplot(data['pushup'], cmap='YlGn', edgecolor='white', figsize=(12, 3.8))
 fig.savefig('result.png')
